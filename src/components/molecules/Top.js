4100b5e7b45eab58d19ab4ae8e49111bb2ac6094
YSSkywalker <yukimurasakiskywalker940716@gmail.com>
2022-10-17 23:51:40 +0900

    Create Top

---
+++ src/components/molecules/Top.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Top.css';
+
+class Top extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>雪丸かるる</h1>
+            </div>
+        );
+    }
+}
+
+export default Top
