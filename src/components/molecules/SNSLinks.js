a3970ad09cef2496d4e7b9343b26841383f3fc2d
YSSkywalker <yukimurasakiskywalker940716@gmail.com>
2023-10-14 05:05:39 +0900

    Add profile photo to sns links

--- src/components/molecules/SNSLinks.js
+++ src/components/molecules/SNSLinks.js
@@ -3,6 +3,9 @@ import './SNSLinks.css';
 import data from './json/snsLinks.json';
 import { FaInstagram, FaLine } from 'react-icons/fa';
 import { FaXTwitter, FaRegHeart, FaTwitch, FaYoutube } from "react-icons/fa6";
+import { BsHouseDoor } from "react-icons/bs"
+
+import ProfileImage from './img/profile_image_sns_links.jpg'
 
 class SNSLinks extends Component {
     constructor(props) {
@@ -18,6 +21,8 @@ class SNSLinks extends Component {
 
     renderIcon(service) {
         switch (service) {
+            case 'Home Page':
+                return <BsHouseDoor />;
             case 'X':
                 return <FaXTwitter />;
             case 'Instagram':
@@ -39,6 +44,12 @@ class SNSLinks extends Component {
         const { links } = this.state;
         return (
             <div className='sns-section'>
+                <div className="container">
+                    <div className="image-wrapper">
+                        <img src={ProfileImage} alt="placeholder" />
+                    </div>
+                    <p className="image-label">Kalulu Yukimaru</p>
+                </div>
                 <div className="sns-links">
                     {links.map((link, index) => (
                         <a key={index} href={`${link.link}${link.userId}`} target="_blank" rel="noopener noreferrer" className="sns-item">
